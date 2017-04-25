7b499ec4d296a6e043616dc8b3982b160ce9f9b6
hank.zheng <uinika@outlook.com>
2017-04-25 12:26:32 +0800

    Code Splitting - CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
-var path = require('path');
+// module.exports = {
+//   module: {
+//     rules: [{
+//       test: /\.css$/,
+//       use: ['style-loader', 'css-loader']
+//     }]
+//   }
+// }
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        use: 'css-loader'
+      })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+  ]
+}

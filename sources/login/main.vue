ddff146e2f1dfcd4fabc2b1d96e63d4d7aae5f90
hank.zheng <zhengh@chinawiserv.com>
2017-06-19 16:36:44 +0800

    Update

--- sources/login/main.vue
+++ sources/login/main.vue
@@ -1,20 +1,34 @@
 <template>
-  <Card class="box-card">
-    <div v-for="o in 4" :key="o" class="text item">
-      {{'列表内容 ' + o }}
-    </div>
-  </Card>
+  <el-card class="box-card">
+    <el-input placeholder="请输入内容" v-model="username">
+      <template slot="prepend">用户名</template>
+    </el-input>
+    <el-input placeholder="请输入内容" v-model="password">
+      <template slot="prepend">密码</template>
+    </el-input>
+  </el-card>
 </template>
 
 <script>
-import { Card } from "element-ui";
+import { Card, Input } from "element-ui";
 
 export default {
-  components: { "Card": Card },
+  components: {
+    "el-card": Card,
+    "el-input": Input
+  },
+  data: function () {
+    return {
+      username: "",
+      password: ""
+    }
+  }
 }
 </script>
 
 
 <style lang="less" scoped>
-
+.el-input {
+  background: red;
+}
 </style>

df89480031c1d7ecd5218ca6884506e7d89c5b2f
hank.zheng <zhengh@chinawiserv.com>
2017-06-19 16:02:20 +0800

    Update

--- sources/login/main.vue
+++ sources/login/main.vue
@@ -1,32 +1,20 @@
 <template>
-  <h1>login</h1>
+  <Card class="box-card">
+    <div v-for="o in 4" :key="o" class="text item">
+      {{'列表内容 ' + o }}
+    </div>
+  </Card>
 </template>
 
 <script>
-import { Table, TableColumn } from "element-ui";
+import { Card } from "element-ui";
 
 export default {
-  components: { "ElTable": Table, TableColumn },
-  data() {
-    return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
-    }
-  }
+  components: { "Card": Card },
 }
 </script>
+
+
+<style lang="less" scoped>
+
+</style>

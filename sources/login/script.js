1fc4e088c5591dcae1f3ed35aa607a919de8fdff
hank.zheng <zhengh@chinawiserv.com>
2017-07-18 18:34:43 +0800

    Update

--- sources/login/script.js
+++ sources/login/script.js
@@ -1,6 +1,25 @@
+import Http from "../common/http.js";
+import Encrypt from "../common/encrypt.js";
 export default {
   data() {
-    return {}
+    return {
+      username: "",
+      password: ""
+    }
   },
-  methods: {}
+  methods: {
+    onSubmit: () => {
+      Http.fetch({
+          method: "post",
+          url: Http.url.master + "/login",
+          data: {
+            loginName: Encrypt.sha(this.data.username),
+            password: Encrypt.sha(this.data.password)
+          }
+        })
+        .then(function (result) {
+          console.log(result)
+        })
+    }
+  }
 };

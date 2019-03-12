658e66b6f5f98c4f7b2813e7a06684f9dba54915
hank.zheng <uinika@outlook.com>
2019-03-12 09:55:45 +0800

    fix issue for dashboard

--- sources/dashboard/script.js
+++ sources/dashboard/script.js
@@ -1,42 +1,50 @@
-import Http from '../common/scripts/http.js';
-import Encrypt from '../common/scripts/encrypt.js';
+import Http from "../common/scripts/http.js";
+import Encrypt from "../common/scripts/encrypt.js";
 export default {
   data() {
     return {
-      cases: [{
-        name: 'Case A',
-        code: '（Judge）0000001',
-        date: '2017/07/21 周五',
-        people: 'some peoples'
-      }, {
-        name: 'Case B',
-        code: '（Judge）0000002',
-        date: '2017/07/21 周五',
-        people: 'some peoples',
-      }, {
-        name: 'Case C',
-        code: '（Judge）0000003',
-        date: '2017/07/21 周五',
-        people: 'some peoples',
-      }, {
-        name: 'Case D',
-        code: '（Judge）0000004',
-        date: '2017/07/21 周五',
-        people: 'some peoples',
-      }],
-      options: [{
-        value: 'yes',
-        label: 'Finished'
-      }, {
-        value: 'no',
-        label: 'Unfinished'
-      }],
-      value: 'yes'
-    }
+      cases: [
+        {
+          name: "Case A",
+          code: "（Demo）0000001",
+          date: "2017/07/21 周五",
+          people: "some peoples"
+        },
+        {
+          name: "Case B",
+          code: "（Demo）0000002",
+          date: "2017/07/21 周五",
+          people: "some peoples"
+        },
+        {
+          name: "Case C",
+          code: "（Demo）0000003",
+          date: "2017/07/21 周五",
+          people: "some peoples"
+        },
+        {
+          name: "Case D",
+          code: "（Demo）0000004",
+          date: "2017/07/21 周五",
+          people: "some peoples"
+        }
+      ],
+      options: [
+        {
+          value: "yes",
+          label: "Finished"
+        },
+        {
+          value: "no",
+          label: "Unfinished"
+        }
+      ],
+      value: "yes"
+    };
   },
   methods: {
     onSearch() {
-      console.log('on Search!');
+      console.log("on Search!");
     }
   }
 };

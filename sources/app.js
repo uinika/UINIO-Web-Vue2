ce4d453c6621cf859ba2f8bd552e642829dcbd09
Hank <uinika@outlook.com>
2019-12-16 16:24:52 +0800

    updated configuration radically

--- sources/app.js
+++ sources/app.js
@@ -1,19 +1,26 @@
-// library
+/** Library */
 import Vue from "vue";
 import Vuex from "vuex";
 import VueRouter from "vue-router";
 import VueRouterSync from "vuex-router-sync";
 import VueI18n from "vue-i18n";
 import "@babel/polyfill";
-// css
+// CSS
+import "animate.css/animate.min.css";
 import "./common/styles/base.scss";
 import "./common/styles/reset.scss";
-// ui
+// Vuetify UI
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.css";
+// Element UI
 import ElementUI from "element-ui";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "@fortawesome/fontawesome-free/js/all.min.js";
 import "element-ui/lib/theme-chalk/index.css";
-// util
+// Fontawesome
+import "@fortawesome/fontawesome-free/js/all.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+/** Helper */
 import Http from "./common/scripts/http.js";
 import Auth from "./common/scripts/auth.js";
 import States from "./common/scripts/store.js";
@@ -24,6 +31,17 @@ Vue.use(Vuex);
 Vue.use(VueRouter);
 Vue.use(VueI18n);
 Vue.use(ElementUI);
+Vue.use(Vuetify);
+
+/** Vuetify Instantiation */
+const vuetify = new Vuetify({
+  theme: {
+    dark: true,
+  },
+  icons: {
+    iconfont: "mdi"
+  },
+});
 
 /** Routers */
 const router = new VueRouter(Routers);
@@ -64,5 +82,6 @@ if (module.hot) {
 const app = new Vue({
   i18n,
   store,
-  router
+  router,
+  vuetify
 }).$mount("#app");
---
+++ sources/demo/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="demo"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

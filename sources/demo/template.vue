9c9d440dbab4017c05b7c3be4ef6cbeae3be4f26
hank.zheng <uinika@outlook.com>
2023-06-08 15:15:17 +0800

    Release UINIO-Web-Vue2 1.0.0.

---
+++ sources/demo/template.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
